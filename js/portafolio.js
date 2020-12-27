041899d2efab1ca41d4a61faac5b4ad7c2f8e844
Bremdow Salazar <bremdows@gmail.com>
2020-12-26 20:41:27 -0500

    Termino del menú lateral
    Se arreglo y rediseño el responsive de la portada, con esto ya estaría terminando parte de la portada que se buscaba construir

--- js/portafolio.js
+++ js/portafolio.js
@@ -3,10 +3,18 @@ document.addEventListener('DOMContentLoaded', ()=>{
     console.log("Documento listo para ejecutar JS");
 
     // Agregando funcionalidad al boton de navegación
-    let barMenu = document.getElementById("bar-nav");
+    let barMenu = document.getElementById("bar-nav"),
+        itemsMenu = document.getElementById('lista-menu');
 
     barMenu.addEventListener('click', ()=>{
-        alert('Hiciste click ser inferior del cual necesito soporte para funcionar correctamente');
+        console.log('Hiciste click ser inferior del cual necesito soporte para funcionar correctamente')
+
+        if(itemsMenu.classList.contains('active')){
+            itemsMenu.classList.remove('active')
+        }else{
+            itemsMenu.classList.add('active')
+        }
+
     });
 
 });
